4f417e33a06542846edaccc5ff204af541761ae5
Raber Sabir <rabersabir@hotmail.com>
2021-10-13 15:37:08 +0200

    Add unit test for the connector

--- app/src/test/java/com/jadept/ns/jokes/connector/JokesConnectorTest.java
+++ app/src/test/java/com/jadept/ns/jokes/connector/JokesConnectorTest.java
@@ -1,12 +1,27 @@
 package com.jadept.ns.jokes.connector;
 
+import com.jadept.ns.jokes.connectors.Joke;
+import com.jadept.ns.jokes.connectors.JokeResponse;
 import com.jadept.ns.jokes.connectors.JokesConnector;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JokesConnectorTest {
@@ -16,10 +31,32 @@ public class JokesConnectorTest {
 
     @Mock
     private RestTemplate restTemplate;
+    private String jokesResourceUrl = "http://localhost/";
+
 
     @Test
-    public  void test(){
+    public void fetchJokes() {
+        ReflectionTestUtils.setField(jokesConnector, "jokesResourceUrl", jokesResourceUrl);
+        JokeResponse jokeResponse = new JokeResponse();
+        jokeResponse.setError(false);
+        Joke joke = new Joke();
+        jokeResponse.setJokes(Arrays.asList(joke));
 
+        ResponseEntity<JokeResponse> response = ResponseEntity.ok(jokeResponse);
+        when(restTemplate.getForEntity(jokesResourceUrl + "any?type=single&amount=16", JokeResponse.class)).thenReturn(response);
+        List<Joke> jokes = jokesConnector.fetchJokes(16, "single");
+
+        assertThat(jokes.size(), CoreMatchers.is(1));
+        assertThat(jokes.get(0), CoreMatchers.is(joke));
+
+    }
+
+
+    @Test(expected = IllegalStateException.class)
+    public void fetchJokesResultsInError() {
+        ReflectionTestUtils.setField(jokesConnector, "jokesResourceUrl", jokesResourceUrl);
+        ResponseEntity<JokeResponse> response = ResponseEntity.internalServerError().build();
+        when(restTemplate.getForEntity(jokesResourceUrl + "any?type=single&amount=16", JokeResponse.class)).thenReturn(response);
         jokesConnector.fetchJokes(16, "single");
     }
 }
--- app/src/test/java/com/jadept/ns/jokes/service/JokesConnectorTest.java
+++
@@ -1,8 +0,0 @@
-package com.jadept.ns.jokes.service;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class JokesConnectorTest {
-}
