84223ad4dc592139a024dda32ac0802f8de9a0a0
Raber Sabir <rabersabir@hotmail.com>
2021-10-13 09:21:17 +0200

    add more unit test

--- app/src/test/java/com/jadept/ns/jokes/service/ExplicitContextServiceTest.java
+++ app/src/test/java/com/jadept/ns/jokes/service/ExplicitContextServiceTest.java
@@ -1,9 +1,15 @@
 package com.jadept.ns.jokes.service;
 
 import com.jadept.ns.jokes.connectors.Joke;
+import org.apache.commons.collections.FastHashMap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Map;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ExplicitContextServiceTest {
@@ -15,6 +21,25 @@ public class ExplicitContextServiceTest {
 
        Joke joke= new Joke();
        joke.setJoke("that is a funny joke");
-       explicitContextService.isJokeAppropriate(joke);
+       assertTrue(explicitContextService.isJokeAppropriate(joke));
+   }
+   @Test
+   public  void notAppropriateJoke(){
+
+      Joke joke= new Joke();
+      joke.setJoke("that is a sex related joke");
+      assertFalse(explicitContextService.isJokeAppropriate(joke));
+   }
+
+
+   @Test
+   public  void oneMoreExplicitJoke(){
+
+      Joke joke= new Joke();
+      joke.setJoke("that is a related joke");
+      Map<String, Boolean> flags= new FastHashMap();
+      flags.put("explicit", true);
+      joke.setFlags(flags);
+      assertFalse(explicitContextService.isJokeAppropriate(joke));
    }
 }
